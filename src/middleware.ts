d8c7ee773d4805714bc6a051ffc5be8b24eae692
594845 <ayandas1821@gmail.com>
2026-05-17 15:58:26 +0530

    \Fix Clerk v6 middleware protect signature syntax to resolve MIDDLEWARE_INVOCATION_FAILED error"

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,10 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isDashboardRoute = createRouteMatcher(['/dashboard(.*)'])
 
-export default clerkMiddleware((auth, req) => {
-  if (isDashboardRoute(req)) auth().protect()
+export default clerkMiddleware(async (auth, req) => {
+  if (isDashboardRoute(req)) {
+    await auth.protect()
+  }
 })
 
 export const config = {
